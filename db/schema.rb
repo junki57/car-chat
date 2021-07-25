9df6b5d678c90c9195010e95f356300bc1bccc38
junki57 <85267609+junki57@users.noreply.github.com>
2021-07-25 16:10:44 +0900

    マイページ機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_053654) do
+ActiveRecord::Schema.define(version: 2021_07_25_065322) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2021_07_25_053654) do
   end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "imageurl"
     t.datetime "created_at", precision: 6, null: false
